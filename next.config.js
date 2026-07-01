300af2906fb50fa6c78a667e7534f1b8f38f6aed
Muhammad <muhammad@example.com>
2026-07-01 23:03:18 +0300

    fix: change next config to js and disable lint on build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   serverExternalPackages: [],
   env: {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY || '',
@@ -18,4 +17,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
